66ca323907f5323a906ad6223b802206b1ccfdb1
Geriano <gerznewbie@gmail.com>
2025-01-26 01:25:12 +0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+
+export default function App() {
+  return (
+    <>
+      <div className="flex items-center justify-center w-screen h-screen bg-gray-200 text-gray-700 dark:bg-gray-800 dark:text-gray-200 font-sans">
+        <h1 className="text-3xl transition-all transform animate-pulse hover:scale-105 duration-700">nusantara.network</h1>
+      </div>
+    </>
+  )
+}
